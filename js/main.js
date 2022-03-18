96ae38bdcb12ea567d6b7e4f51cafaf6bcfae5c3
oliver-wendel-nti-johanneberg <74712544+oliver-wendel-nti-johanneberg@users.noreply.github.com>
2022-03-18 14:44:27 +0100

    Fixade de flesta funktionerna dock behövs matte funktionen fixas

--- js/main.js
+++ js/main.js
@@ -11,24 +11,16 @@ var operator_array = document.getElementsByClassName('operator')
 var equal_array = document.getElementsByClassName('eql') 
 let input_box_array = document.getElementsByClassName('textbox')
 let button = document.getElementById('button_box')
-let value = []
+let value = [];
+var answer_array = [];
+var num1_array_in = [];
+var num2_array_in = [];
+
+
 
 
 // document.getElementsByClassName('operator').innerText = '+';
 
-button.addEventListener(`click`, () => { 
-
-    let value = []
-
-    for (var i = 0; i < input_box_array.length; i++) {
-        value[i] = input_box_array[i].value
-    }
-
-    console.log(value)
-
-    return value
-
-})
 
 function getinput() {
     return input_array = document.getElementsByClassName('textbox')
@@ -39,11 +31,13 @@ function randomnum(min, max){
 }
 
 for (var i = 0; i < num1_array.length; i++) {
-    num1_array[i].innerHTML = randomnum(min, max)
+    num1_array_in[i] = randomnum(min, max)
+    num1_array[i].innerHTML = num1_array_in[i]
 }
 
 for (var i = 0; i < num2_array.length; i++) {
-    num2_array[i].innerHTML = randomnum(min, max)
+    num2_array_in[i] = randomnum(min, max)
+    num2_array[i].innerHTML = num2_array_in[i]
 }
 
 for (var i = 0; i < operator_array.length; i++) {
@@ -55,30 +49,76 @@ for (var i = 0; i < equal_array.length; i++) {
 }
 
 var math = {
-    '+': function (x, y) { return x + y },
+    '+': function (x, y) { x + y },
     '-': function (x, y) { return x - y },
     '*': function (x, y) { return x * y },
     '/': function (x, y) { return x / y }
 }
 
-
-var answer_array = [];
 function answers(num1_array, num2_array, user_input) {
     for (var i = 0; i < num1_array.length; i++) {
-
       answer_array[i] =  math[user_input](num1_array[i], num2_array[i])
     }
     return answer_array
 }
 
-function toggleCheckmark() {
-    if function() == true
-        document.querySelector('.check').classList.toggle('correctmark')
-    else
-        document.querySelector('check').classList.toggle('falsemark')
+function check_answers(value_array, answer_array) {
+
+    var result_array = []
+
+    let check_array = document.getElementsByClassName('check')
+
+
+    for (var i = 0; i < answer_array.length; i++) {
+        console.log(value_array[i])
+        console.log(answer_array[i])
+        parseInt(value_array[i])
+        if (value_array[i] == answer_array[i]) {
+            console.log('rätt svar')
+            result_array[i] = 1
+            check_array[i].classList.toggle('correctmark')
+        }
+        else {
+            console.log('er her')
+            result_array[i] = 0
+            check_array[i].classList.toggle('falsemark')
+        }
+    }
+
+    return result_array
+
 }
 
 
 
+function draw(result_array) {
+    if (result_array[0] == 1) {
+        document.querySelector('.check').classList.toggle('correctmark')
+    }
+    else {
+        document.querySelector('.check').classList.toggle('falsemark')
+    }
+}
+
+console.log(num1_array)
+console.log(num2_array)
+
+button.addEventListener(`click`, (num1_array, num2_array, user_input) => { 
+
+    let value_array = []
+
+    for (var i = 0; i < input_box_array.length; i++) {
+        value_array[i] = input_box_array[i].value
+    }
+
+    console.log(value_array)
+
+    console.log(num1_array_in)
+    console.log(num2_array_in)
+    console.log(user_input) 
+
+    check_answers(value_array, answers(num1_array_in, num2_array_in, user_input))
+
+})
 
 console.log(answers(num1_array, num2_array, user_input))
