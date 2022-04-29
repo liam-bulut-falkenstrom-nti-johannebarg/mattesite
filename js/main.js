5bb3ab6cd9f42d1b295c0e86c5ab918fc0dd04bb
erik-greenblomroos-nti-johanneberg <74712714+erik-greenblomroos-nti-johanneberg@users.noreply.github.com>
2022-04-29 10:59:04 +0200

    Co-authored-by: oliver-wendel-nti-johanneberg <oliver-wendel-nti-johanneberg@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -21,15 +21,16 @@ let has_answered = false;
 var answer_array = [];
 var num1_array_in = [];
 var num2_array_in = [];
+var stat_array = [];
 var stat_array_add = [];
 var stat_array_sub = [];
 var stat_array_mul = [];
 var stat_array_div = [];
-var game_log_add = [];
-var game_log_sub = [];
-var game_log_mul = [];
-var game_log_div = [];
-var last_result = 0;
+let game_log_add = [];
+let game_log_sub = [];
+let game_log_mul = [];
+let game_log_div = [];
+let last_result = 0;
 let selector = document.getElementsByClassName('type_button')
 let selector1 = document.getElementsByClassName('type_button1')
 let selector2 = document.getElementsByClassName('type_button2')
@@ -39,7 +40,9 @@ let sub_select = document.getElementsByClassName('sub_button')
 let div_select = document.getElementsByClassName('div_button')
 let mul_select = document.getElementsByClassName('mul_button')
 let add_select = document.getElementsByClassName('add_button')
-
+let stats = document.getElementsByClassName('stat_list')
+let show = document.querySelector('.result_board')
+let time_used = 0;
 try {
     selector1[0].addEventListener('click', () => {
         user_input = "+";
@@ -81,18 +84,19 @@ catch (error) {
     
 }
 
-var start_timer = 10
+var start_timer = 30.00
 
-var count_down_timer = 10
+var count_down_timer = 30.00
+
 
 var time_tracker = setInterval(function() {
     try {
-        time_select.innerHTML = "Time: " + count_down_timer 
-        
-        if (count_down_timer < 0) {
+        if (has_answered != true) {
+        time_select.innerHTML = "Time: " +  count_down_timer.toFixed(2)
+        if (count_down_timer < 0 || has_answered) {
         clearInterval(time_tracker);
         let value_array = []
-        count_down_timer -= 1
+        count_down_timer -= 0.01
 
             for (var i = 0; i < input_box_array.length; i++) {
                 value_array[i] = input_box_array[i].value
@@ -103,19 +107,25 @@ var time_tracker = setInterval(function() {
         console.log(num1_array_in)
         console.log(num2_array_in)
         console.log(user_input) 
+        
 
         check_answers(value_array, answers(num1_array_in, num2_array_in, user_input))
         has_answered = true
-        time_result.innerHTML = start_timer - count_down_timer
+        time_used = start_timer - count_down_timer
+        console.log(time_used)
+        console.log(time_used.toFixed(2))
+        time_result.innerHTML = time_used.toFixed(2)
         time_select.innerHTML = "Slut på tid";
+        show.classList.toggle('hidden_class')
         }
         else {
-            count_down_timer -= 1
+            count_down_timer -= 0.01
         }
+    }
     }
     catch (error) {
     }
-}, 1000);
+}, 10);
 
 function getinput() {
     return input_array = document.getElementsByClassName('textbox')
@@ -180,13 +190,11 @@ function check_answers(value_array, answer_array) {
         console.log(answer_array[i])
         parseInt(value_array[i])
         if (value_array[i] == answer_array[i]) {
-            console.log('rätt svar')
             result_array[i] = 1
             check_array[i].classList.toggle('correctmark')
             last_result += 1
         }
         else {
-            console.log('er her')
             result_array[i] = 0
             check_array[i].classList.toggle('falsemark')
         }
@@ -207,7 +215,7 @@ function check_answers(value_array, answer_array) {
 //     }
 // }
 try {
-button.addEventListener(`click`, (num1_array_in, num2_array_in, user_input) => { 
+button.addEventListener(`click`, (num1_array_in, num2_array_in) => { 
     if (has_answered != true){
         let value_array = []
 
@@ -225,6 +233,18 @@ button.addEventListener(`click`, (num1_array_in, num2_array_in, user_input) => {
         time_result_i = start_timer - count_down_timer
         time_result.innerHTML = time_result_i
         has_answered = true
+
+        console.log(user_input)
+        show.classList.toggle('hidden_class')
+        stat_array = stats_log(user_input)
+        console.log(game_log_add)
+        console.log(stat_array)
+        console.log(stats)
+
+        for (var i = 0; i < stat_array.length; i++) {
+            stats[i].innerHTML = stat_array[i]
+        }
+        
     }
 })
 }
@@ -234,25 +254,38 @@ catch (error) {
 
 function stats_log(mode) {
     if (mode == "+") {
+        console.log("inne i mode")
+        if (localStorage.getItem("stat_log_add") != null) {
+            game_log_add = localStorage.getItem("stat_log_add");
+        }
         tio_senaste = 0.0
         tre_senaste = 0.0
-        game_log_add << [time_result_i, last_result]
-        if (game_log_add.length > 10) {
-            game_log_add.shift()
-        }
-        for (var i = 9; i > -1; i--) {
-            antal_fel = 12 - game_log_add[i][0]
+        antal_fel = 0
+        antal_ratt = 0
+        procent_ratt = 0
+        console.log(last_result)
+        console.log(time_used)
+        console.log(time_result)
+        console.log(game_log_add)
+        game_log_add[game_log_add.length] = [time_result_i, last_result];
+        console.log(game_log_add)
+        // if (game_log_add.length > 10) {
+        //     game_log_add.shift()
+        // }
+        for (var i = game_log_add.length-1; i > -1; i--) {
+            antal_fel = 10 - game_log_add[i][0]
             antal_ratt = game_log_add[i][0]
-            procent_ratt = parseFloat(antal_ratt / antal_fel)
+            procent_ratt = (parseFloat(antal_ratt / antal_fel)*100).toFixed(1)
             if (i == 6) {
                 tre_senaste = tio_senaste
             }
             tio_senaste += procent_ratt
         }
-        antal_fel = 12 - last_result
+        antal_fel = 10 - last_result
         antal_ratt = last_result
-        procent_ratt = parseFloat(antal_ratt / antal_fel)
-        stat_array_add = [antal_ratt, antal_fel, procent_ratt, tre_senaste, tio_senaste]
+        procent_ratt = (parseFloat(antal_ratt / antal_fel)*100).toFixed(1)
+        localStorage.setItem("stat_log_add", game_log_add);
+        return [antal_ratt, antal_fel, procent_ratt, tre_senaste, tio_senaste]
     }
     else if (mode == "-") {
 
@@ -265,6 +298,7 @@ function stats_log(mode) {
     }
 }
 
+console.log(has_answered)
 console.log(answers(num1_array_in, num2_array_in, user_input))
 console.log(answers(num1_array_in, num2_array_in, user_input))
 console.log(input_array)
